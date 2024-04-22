66b91db88b5c6c6123955d772d14db45ae35fb4a
Anatolii Golikov <tgolikov03@mail.ru>
2024-04-22 21:14:53 +0300

    feat: arrows

--- src/components/common/Gallery/Gallery.tsx
+++ src/components/common/Gallery/Gallery.tsx
@@ -11,53 +11,52 @@ import { useEffect, useRef, useState } from 'react';
 import img from '@/assets/img/test.jpg'
 import img2 from '@/assets/img/test2.jpg'
 import SliderItem from './components/SliderItem/SliderItem';
-import useWindowDimensions from '@/hooks/window-dimension';
 import { useContainerDimensions } from '@/hooks/container-dimenstion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Gallery() {
   const sliderContainerRef = useRef<any>()
-  const {width} = useContainerDimensions(sliderContainerRef)
-
-  const slidesPerPage = 7
-  const centerSlideWidth = 640
-  const slideWidth = ((width - centerSlideWidth - 10) / (slidesPerPage - 1)) - 10
-  console.log(slideWidth)
-
+  const { width } = useContainerDimensions(sliderContainerRef)
   const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0)
   const slides = useRef<Element[]>([])
+
+  const slidesPerPage = 7
+  const centerSlideWidth = width / 3
+  const slideWidth = ((width - centerSlideWidth - 10) / (slidesPerPage - 1)) - 10
+
+
 
   const sliderInit = () => {
     const s = document.getElementsByClassName('slider-item');
     const containerStyle = (document.getElementsByClassName('slider-container')[0] as any).style as CSSStyleDeclaration
     const oldActiveSlide = document.getElementsByClassName('slider-item-active')[0]
-    console.log(oldActiveSlide)
-    if(oldActiveSlide) {
+    if (oldActiveSlide) {
       oldActiveSlide.classList.remove('slider-item-active')
     }
 
     slides.current = [...s]
     const index = Math.floor(s.length / 2);
     setActiveSlideIndex(index);
-    
+
     (s[index] as any).style.minWidth = `${centerSlideWidth}px`
     s[index].classList.add('slider-item-active')
-    containerStyle.transform = `translate3d(-${slideWidth+10}px, 0px, 0px)`
+    containerStyle.transform = `translate3d(-${slideWidth + 5}px, 0px, 0px)`
   }
 
   const changeSlide = (index: number) => {
-    if(index < 0) {
+    if (index < 0) {
       return
-    } else if(index > slides.current.length - 1) {
+    } else if (index > slides.current.length - 1) {
       return
     }
 
-    const direction = index > activeSlideIndex ? 1 : -1
-
+    const direction = index - activeSlideIndex
     const elementStyle = (document.getElementsByClassName('slider-container')[0] as any).style as CSSStyleDeclaration
 
     const px = Number(elementStyle.transform.match(/translate3d\((-?\d+(?:\.\d+)?px)/)![1].replace('px', '')) - direction * slideWidth - direction * 10
     elementStyle.transform = elementStyle.transform.replace(/(-?\d+(?:\.\d+)?px)/, `${px.toString()}px`)
-    //elementStyle.transform = `translate3d(-${(slideWidth+10)*index}px, 0px, 0px)`
     setActiveSlideIndex(index);
 
     (slides.current[index - direction] as any).style.minWidth = `${slideWidth}px`;
@@ -80,54 +79,25 @@ export default function Gallery() {
 
 
   return (
-    <>
-      <div className='text-white'>
-        {activeSlideIndex.toString()}
+    <div className='slider-block-container'>
+      <div className='slider-arrows-container'>
+        <FontAwesomeIcon className='slider-arrows-item' icon={faArrowLeft} onClick={(e) => goBack(activeSlideIndex)} />
+        <FontAwesomeIcon className='slider-arrows-item' icon={faArrowRight} onClick={(e) => goForward(activeSlideIndex)} />
       </div>
-      <button onClick={(e) => goBack(activeSlideIndex)}>Back</button>
-        <button onClick={(e) => goForward(activeSlideIndex)}>Forward</button>
       <div className='slider-overflow-container' ref={sliderContainerRef}>
         <div className='slider-container text-white' style={{ transform: 'translate3d(0px, 0px, 0px)' }}>
-          <SliderItem imgSrc={img} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img2} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img2} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img2} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img2} style={{ minWidth: slideWidth }} />
-          <SliderItem imgSrc={img} style={{ minWidth: slideWidth }} />
+          {new Array(9).fill(0).map((nothing: any, index: number) => (
+            <SliderItem
+              imgSrc={[img, img2]}
+              style={{ minWidth: slideWidth }}
+              isActive={activeSlideIndex === index}
+              index={index}
+              key={index}
+              onClick={(index) => changeSlide(index)}
+            />
+          ))}
         </div>
       </div>
-
-
-
-
-      {/* <div style={{height: '25rem'}}>
-        <Swiper
-          slidesPerView={7}
-          spaceBetween={30}
-          centeredSlides={true}
-          navigation={true} 
-          modules={[Navigation]}
-          className="gallery-slider"
-          onSlideChangeTransitionEnd={onSlideChange}
-          ref={swiper}
-        >
-          <SwiperSlide >Slide 1</SwiperSlide>
-          <SwiperSlide >Slide 2</SwiperSlide>
-          <SwiperSlide >Slide 3</SwiperSlide>
-          <SwiperSlide >Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-          <SwiperSlide>Slide 10</SwiperSlide>
-          <SwiperSlide>Slide 11</SwiperSlide>
-        </Swiper>
-      </div> */}
-
-    </>
+    </div>
   );
 }
--- src/components/common/Gallery/components/SliderItem/SliderItem.tsx
+++ src/components/common/Gallery/components/SliderItem/SliderItem.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 import './SliderItem.scss'
 
 interface PropsSliderItem {
   style: React.CSSProperties,
-  imgSrc: string
+  imgSrc: string[]
+  index: number
+  isActive: boolean
+  onClick?: (index: number) => void
 }
 
-export default function SliderItem({style, imgSrc}: PropsSliderItem) {
+export default function SliderItem({ style, imgSrc, isActive, index, onClick }: PropsSliderItem) {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+  const onItemClick = (index: number) => {
+    setCurrentImageIndex(index)
+  }
+
   return <>
     <div className='slider-item' style={style}>
-      <img src={imgSrc}></img>
+      {isActive &&
+        <div className='slider-item-favicons-container text-white'>
+          {
+            imgSrc.map((src, index) => (
+              <div className={`slider-item-favicons-item ${ index === currentImageIndex ? 'slider-item-favicons-item-active' : '' }`} key={index} onClick={() => onItemClick(index)}>
+                <img src={src} className='slider-item-favicons-img' />
+              </div>
+            ))
+          }
+        </div>
+      }
+      <img className='slider-item-main-image' src={imgSrc[currentImageIndex]} onClick={() => onClick && onClick(index)}></img>
     </div>
   </>
 }
