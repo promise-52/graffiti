cc08e4e5bd674479f77d371531a64366dfe05bbc
AllexKzk <san4a_kozikov@mail.ru>
2024-06-12 21:04:52 +0300

    feat: navbar responsive

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import './Header.scss';
+import { faBars, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function NavBar() {
   const tabs = [
@@ -22,25 +24,37 @@ export function NavBar() {
   ];
 
   const [lang, setLang] = useState<'ru' | 'eng'>('ru');
+  const [isVisible, setVisible] = useState(false);
+
   return (
-    <div className=" text-pptelegraph navbar">
-      <div className="navbar-links">
-        {
-          tabs.map((tab) => 
-            <a href={`#${tab.yakor}`}>
-              { tab.label }
-            </a>
-          )
-        }
+    <>
+      <div style={{ display: 'flex' }}>
+        <FontAwesomeIcon
+          icon={!isVisible ? faBars : faChevronLeft}
+          className='open-icon'
+          onClick={() => setVisible(!isVisible)}
+        />
       </div>
-      <div className="navbar-lang">
-        <a className={ lang === 'eng' ? 'active' : '' } onClick={() => setLang('eng')} href="#">
-          ENG
-        </a>
-        <a className={ lang === 'ru' ? 'active' : '' } onClick={() => setLang('ru')} href="#">
-          RU
-        </a>
+
+      <div className={`text-pptelegraph navbar ${isVisible ? 'visible' : ''}`}>
+        <div className="navbar-links">
+          {
+            tabs.map((tab) => 
+              <a href={`#${tab.yakor}`}>
+                { tab.label }
+              </a>
+            )
+          }
+        </div>
+        <div className="navbar-lang">
+          <a className={ lang === 'eng' ? 'active' : '' } onClick={() => setLang('eng')} href="#">
+            ENG
+          </a>
+          <a className={ lang === 'ru' ? 'active' : '' } onClick={() => setLang('ru')} href="#">
+            RU
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
